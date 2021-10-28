fc3dea5f6f762b6a074659e7095e1b8b17d3c511
Gonzalo Ovalle <gonzalo.ovalle01@gmail.com>
2021-10-28 16:44:03 -0500

    handling form submission practice

---
+++ src/main/java/com/example/handlingformsubmission/Controllers/GreetingController.java
@@ -0,0 +1,26 @@
+package com.example.handlingformsubmission.Controllers;
+
+
+import com.example.handlingformsubmission.Models.Greeting;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class GreetingController {
+
+    @GetMapping("/greeting")
+    public String greetingForm(Model model) {
+        model.addAttribute("greeting", new Greeting());
+        return "greeting";
+    }
+
+    @PostMapping("/greeting")
+    public String greetingSubmit(@ModelAttribute Greeting greeting, Model model) {
+        model.addAttribute("greeting", greeting);
+        return "result";
+    }
+
+}
